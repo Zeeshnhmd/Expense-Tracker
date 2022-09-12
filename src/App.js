efef92bacd02034682808dd4aed3781c908f164d
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-09-12 16:45:41 +0530

    test github

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-	return <div className="App">Expense tracker testing git</div>;
+	return <div className="App">Expense tracker testing github</div>;
 }
 
 export default App;
