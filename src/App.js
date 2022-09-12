19cf2bf9d1095a548a143cf254bf8427d8cab465
Zeeshan Personal <zeeshanpersonal@Zeeshans-MacBook-Pro.local>
2022-09-12 16:41:57 +0530

    test git

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-	return <div className="App">Expense tracker testing github</div>;
+	return <div className="App">Expense tracker testing git</div>;
 }
 
 export default App;
