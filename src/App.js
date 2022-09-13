54b5e2c475d2d7a071fa034aab2bbeaaeb3dfe74
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-09-13 11:25:01 +0530

    added header and balance component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
 
 function App() {
-	return <div className="App">Expense tracker 0.1.1</div>;
+	return (
+		<div>
+			<Header header="Expense Tracker" />
+			<div className="container">
+				<Balance />
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../App.css';
+
+const Header = ({ header }) => {
+	return <h2>{header}</h2>;
+};
+
+export default Header;
