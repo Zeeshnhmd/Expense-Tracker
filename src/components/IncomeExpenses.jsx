2bd90c10cb648f87f05277249ebe7991b0f50459
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-09-13 13:36:45 +0530

    changes $ to rupees mapped the transactions array created transaction component

--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import GlobalState from '../context/GlobalState';
 
 const IncomeExpenses = () => {
+	const { transactions } = useContext(GlobalState);
+	const amount = transactions.map((transaction) => transaction.amount);
+	const total = amount.reduce((a, b) => (a += b), 0).toFixed(2);
+
+	/**
+	 * * here we are using toFixed(2) because
+	 */
+
 	return (
 		<div className="inc-exp-container">
 			<div>
 				<h4>Income</h4>
-				<p className="money plus">+$0.00</p>
+				<p className="money plus">+&#8377;{total}</p>
 			</div>
 			<div>
 				<h4>Expense</h4>
-				<p className="money minus">-$0.00</p>
+				<p className="money minus">-&#8377;0.00</p>
 			</div>
 		</div>
 	);
--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import GlobalState from '../context/GlobalState';
+import Transaction from './Transaction';
 
 const TransactionList = () => {
 	const { transactions } = useContext(GlobalState);
@@ -8,10 +9,9 @@ const TransactionList = () => {
 		<>
 			<h3>History</h3>
 			<ul className="list">
-				<li className="minus">
-					Cash <span>-$400</span>
-					<button className="delete-btn">x</button>
-				</li>
+				{transactions.map((transaction) => (
+					<Transaction key={transaction.id} transaction={transaction} />
+				))}
 			</ul>
 		</>
 	);
