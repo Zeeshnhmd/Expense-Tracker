141755f41f40340027c3737056d378f630239c44
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-09-13 16:02:48 +0530

    income expenses value

--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import GlobalState from '../context/GlobalState';
 
 const IncomeExpenses = () => {
+	const { transactions } = useContext(GlobalState);
+	const amount = transactions.map((transaction) => transaction.amount);
+
+	const income = amount
+		.filter((item) => item > 0)
+		.reduce((a, b) => (a += b), 0)
+		.toFixed(2);
+
+	const expense = amount
+		.filter((item) => item < 0)
+		.reduce((a, b) => (a += b), 0)
+		.toFixed(2);
+
 	return (
 		<div className="inc-exp-container">
 			<div>
 				<h4>Income</h4>
-				<p className="money plus">+&#8377;</p>
+				<p className="money plus">&#8377; {income}</p>
 			</div>
 			<div>
 				<h4>Expense</h4>
-				<p className="money minus">-&#8377;0.00</p>
+				<p className="money minus">&#8377;{Math.abs(expense)}</p>
 			</div>
 		</div>
 	);
